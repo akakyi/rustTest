15db0a430d51465127250f8daf4e5429d9539a96
Fomin Mikhail <m.fomin@elama.ru>
2024-02-04 16:26:40 +0400

    something

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 //use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Write \"Test\"");
@@ -14,5 +16,18 @@ fn main() {
         println!("Da")
     } else {
         println!("Zochem {input}?")
+    }
+
+    let secretNumber = rand::thread_rng().gen_range(1..=100);
+    let input = input.trim().parse::<i32>().expect("That is not a number!");
+//    if (inputInt == secretNumber) {
+//        println!("Ugadal")
+//    } else {
+//        println!("Ne ugadal)))00)");
+//    }
+    match input.cmp(&secretNumber) {
+        Ordering::Less => println!("You entered less"),
+        Ordering::Equal => println!("Ugadal)))00)"),
+        Ordering::Greater => println!("You entered less")
     }
 }
